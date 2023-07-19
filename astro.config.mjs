f9fa17d9d73745e7c8a0a9115dbe94783a9aae66
DanielTate <dan@tate.nz>
2023-07-19 13:34:14 +1200

    Update config for gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+    base: 'oauth-docs/',
     integrations: [
         starlight({
             title: 'AURA Oauth documentation',
